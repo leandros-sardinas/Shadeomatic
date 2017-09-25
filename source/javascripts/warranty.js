a5fa6ededa3c176c7d3d3ccf5a7b705124a8fd22
lsardinas <lsardinas@changewings.com>
2017-09-25 08:09:49 -0400

    changes

--- source/javascripts/warranty.js
+++ source/javascripts/warranty.js
@@ -4,10 +4,12 @@ $(document).ready(function() {
         data: {
             error: '',
             inError: '',
+            loading: false,
 
             contentUrl: '/db/countries.json',
             countries: [],
-            selectedCountry: {},
+            provinces: [],
+            
             order:'',
             name: '',
             email: '',
@@ -18,12 +20,46 @@ $(document).ready(function() {
             city: '',
             postalCode: ''
         },
-        mounted: function() {
+        created: function() {
             this.loadCountries();
         },
         methods: {
-            sendWarranty: function(e) {                
-                $('#app').validate();
+            sendWarranty: function(e) {
+                var self = this;
+
+                if (!$('#app').valid()) {
+                    return;
+                }
+
+                //Set loading
+                self.loading = true;
+
+                //Form url to register warranty
+                var url = '';
+
+                //Register data
+                $.ajax({
+                    url: '',
+                    error: function() {
+                        self.inError = true;
+                        self.error = appMessages.Messages.generalError;
+                    }
+                }).done(function(data) {
+
+                }).always(function(){
+                    self.loading = false;
+                });
+
+                console.log(this.order);
+                console.log(this.name);
+                console.log(this.email);
+                console.log(this.country);
+                console.log(this.province);
+                console.log(this.address);
+                console.log(this.address2);
+                console.log(this.city);
+                console.log(this.postalCode);
+
                 e.preventDefault();
             },
             loadCountries: function() {
@@ -37,8 +73,23 @@ $(document).ready(function() {
                     }
                 }).done(function(data) {
                     self.countries = data.countries;
-                    self.selectedCountry = self.countries[0];
+                    self.country = self.countries[0].abbrv;
+                    self.provinces = self.countries[0].provinces;
+                    self.province = self.countries[0].provinces[0].abbrv;
                 })
+            }
+        },
+        watch: {
+            country: function(val) {
+                for(var i=0; i<this.countries.length; i++) {
+                    if (this.countries[i].abbrv === val) {
+                        this.country = this.countries[i].abbrv;
+                        this.provinces = this.countries[i].provinces;                        
+                        this.province = this.countries[i].provinces[0].abbrv;
+
+                        return;
+                    }
+                }                
             }
         }
     });
